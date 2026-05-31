3a0aea334003b4d2836032c754b2848b291e8da1
grumble-person <danacstarr@gmail.com>
2026-05-30 20:53:21 -0400

    more logs to help find what's going on

--- calc.js
+++ calc.js
@@ -44,7 +44,9 @@ function rollFactions() {
 	// there is an extremely low chance of the eyrie being selected when every low reach faction is in the pool
 	// this gives them a better shot by truncating the pool of factions that can never be selected
 	if (playerCount == 2) {
-		for (let i = arrayPool.length-1; i < -1; i--) {
+		console.log ("we should truncate for 2 players");
+		for (let i = (arrayPool.length-1); i < -1; i--) {
+			console.log(arrayPool[i].factionName + " has " + arrayPool[i].factionReach + ", we need at least " + reachViable-10);
 			if (arrayPool[i].factionReach < (reachViable - 10)) {
 				console.log("truncating for 2p... goodbye " + arrayPool[i].factionName);
 				arrayPool.splice(i, 1);
