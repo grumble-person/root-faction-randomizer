e625f259d41bf5a3969d6bef3aa367cf5701a302
grumble-person <danacstarr@gmail.com>
2026-05-30 21:19:21 -0400

    removed recursion, fixed permanent 2 reach vagabond

--- calc.js
+++ calc.js
@@ -33,46 +33,63 @@ function roll() {
 }
 
 function rollFactions() {
-	initPool();
-	let playerCount = document.querySelector("input[type=radio][name=playerCount]:checked").value;
-	console.log("players selected: " + playerCount);
-	let reachViable = arrayReach[playerCount - 2];
-	let reachSum = 0;
-	console.log("players selected: " + playerCount + "; reach needed: " + reachViable);
-	arraySelected = [];
-
-	// there is an extremely low chance of the eyrie being selected when every low reach faction is in the pool
-	// this gives them a better shot by truncating the pool of factions that can never be selected
-	if (playerCount == 2) {
-		console.log ("we should truncate for 2 players");
-		for (let i = (arrayPool.length-1); i > -1; i--) {
-			console.log(arrayPool[i].factionName + " has " + arrayPool[i].factionReach + ", we need at least " + reachViable-10);
-			if (arrayPool[i].factionReach < (reachViable - 10)) {
-				console.log("truncating for 2p... goodbye " + arrayPool[i].factionName);
-				arrayPool.splice(i, 1);
+	let successfulRoll = false;
+	let rolls = 0;
+	while (successfulRoll == false) {
+		rolls = rolls + 1;
+		initPool();
+		let playerCount = document.querySelector("input[type=radio][name=playerCount]:checked").value;
+		console.log("players selected: " + playerCount);
+		let reachViable = arrayReach[playerCount - 2];
+		let reachSum = 0;
+		let anotherVagabond = false;
+		console.log("players selected: " + playerCount + "; reach needed: " + reachViable);
+		arraySelected = [];
+	
+		// there is an extremely low chance of the eyrie being selected when every low reach faction is in the pool
+		// this gives them a better shot by truncating the pool of factions that can never be selected
+		if (playerCount == 2) {
+			console.log ("we should truncate for 2 players");
+			for (let i = (arrayPool.length-1); i > -1; i--) {
+				console.log(arrayPool[i].factionName + " has " + arrayPool[i].factionReach + ", we need at least " + reachViable-10);
+				if (arrayPool[i].factionReach < (reachViable - 10)) {
+					console.log("truncating for 2p... goodbye " + arrayPool[i].factionName);
+					arrayPool.splice(i, 1);
+				}
 			}
 		}
-	}
-	
-	while (arraySelected.length < playerCount) {
-		let rand = Math.floor(Math.random() * arrayPool.length);
-		reachSum = reachSum + arrayPool[rand].factionReach;
-		console.log("trying " + arrayPool[rand].factionName + ", reachSum = " + reachSum);
-		arraySelected.push(Object.assign({}, arrayPool[rand]));
-		// after first finding vagabond, this makes all future instances of vagabond reach 2, even in new rolls, FIX
-		if (arrayPool[rand].factionName == "Vagabond" && arrayPool[rand].factionReach == 5) {
-			arrayPool[rand].factionReach = 2;
-		} else {
-			arrayPool.splice(rand, 1);
+		
+		while (arraySelected.length < playerCount) {
+			let rand = Math.floor(Math.random() * arrayPool.length);
+			reachSum = reachSum + arrayPool[rand].factionReach;
+			console.log("trying " + arrayPool[rand].factionName + ", reachSum = " + reachSum);
+			arraySelected.push(Object.assign({}, arrayPool[rand]));
+			// after first finding vagabond, this makes all future instances of vagabond reach 2, even in new rolls, FIX
+			if (arrayPool[rand].factionName == "Vagabond" && anotherVagabond == true) {
+				reachSum = reachSum - 3;
+				arrayPool.splice(rand, 1);
+				console.log("another vagabond already! -3. reachSum = " + reachSum);
+			} else if (arrayPool[rand].factionName == "Vagabond" && anotherVagabond == false) {
+				anotherVagabond = true;
+			} else {
+				arrayPool.splice(rand, 1);
+			}
 		}
-	}
 
-	if (reachSum < reachViable) { //FIX RECURSION
-		console.log('Reach too low; retrying...');
-		rollFactions();
-	} else {
-		return;
+		if (reachSum < reachViable) {
+			console.log('Reach too low; retrying...');
+		} else {
+			successfulRoll = true;
+			console.log("we did it in " + rolls + " rolls!");
+		}
+		
 	}
+	//if (reachSum < reachViable) { //FIX RECURSION
+	//	console.log('Reach too low; retrying...');
+	//	rollFactions();
+	//} else {
+	//	return;
+	//}
 }
 
 function drawFactions() {
